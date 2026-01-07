8238bab026c9aa94a92f052872dc2c7006d111d1
Andranik Simonian <andy.7imonian@gmail.com>
2026-01-07 00:01:41 +0000

    Basic Configuration & Starting Automation

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,20 +1,12 @@
-// @ts-check
-import { test, expect } from '@playwright/test';
-import {LoginPage} from "../pages/LoginPage";
+import { test } from '@playwright/test';
 
-test.describe(`Login tests`, () => {
+test.describe('User authentication', () => {
 
-  test.beforeEach(async ({page}) => {
-    await page.goto('https://qa.koel.app/')
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/');
   });
 
-  test(`User logs in @smoke`, async ({page}) => {
-    const loginPage = new LoginPage(page);
-    await loginPage.enterEmail("oleg@testpro.io")
-    await loginPage.enterPassword("R4Swbxexv$yqQ9W1")
-    await loginPage.clickLogIn()
-
-    // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle(/Koel/);
+  test('User logs in', async ({ page }) => {
+    // test steps...
   });
 });
