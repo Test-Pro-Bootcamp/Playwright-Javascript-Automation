cd3810997cee44eb6b45011dc04c6d89fbb499a7
Andranik Simonian <andy.7imonian@gmail.com>
2026-01-10 07:17:41 +0000

    Test Annotations

--- tests/playlist.spec.js
+++ tests/playlist.spec.js
@@ -1,26 +1,11 @@
+import { test } from '@playwright/test';
 
-import { test, expect } from '@playwright/test';
-import { randomInt } from 'node:crypto';
-
-test.describe('Playlist management', () => {
-
-    test.beforeEach(async ({ page }) => {
-        await page.goto('/');
-        await page.getByPlaceholder('Email Address').fill('andranik@testpro.io');
-        await page.getByPlaceholder('Password').fill('Surenchik228.');
-        await page.getByRole('button', { name: 'Log In' }).click();
-        await expect(page).toHaveURL(/#!\/home/);
-        await expect(page.getByText("Your Music")).toBeVisible();
-    });
-
+test.describe(
+  'Playlist management',
+  { tag: '@smoke' },
+  () => {
     test('create a new playlist', async ({ page }) => {
-        await page.getByTestId('sidebar-create-playlist-btn').click();
-        await page.getByTestId('playlist-context-menu-create-simple').click();
-    
-        const playlistName = 'My Favorite Songs' + randomInt(100000);
-        await page.getByPlaceholder('↵ to save').fill(playlistName);
-        await page.keyboard.press('Enter');
-
-        await expect(page.locator(`//section[@id="playlists"]//a[.='${playlistName}']`)).toBeVisible();
+      // test logic will be added later
     });
-});
+  }
+);
