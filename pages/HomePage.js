5e380c033fcbec972edeb51939386922ce275abb
Andranik Simonian <andy.7imonian@gmail.com>
2026-01-11 00:40:03 +0000

    Playwright: POM

---
+++ pages/HomePage.js
@@ -0,0 +1,23 @@
+import BasePage from "./BasePage";
+import { expect } from '@playwright/test';
+
+export default class HomePage extends BasePage {
+  constructor(page) {
+    super(page);
+    this.userProfileBtn = this.page.getByTestId('view-profile-link')
+  };
+
+  static get url() {
+    return '/#!/home';
+  };
+
+  async expectHomePageIsOpened() {
+    await expect(this.page).toHaveURL(HomePage.url);
+    await expect(this.userProfileBtn).toBeVisible();
+  };
+
+  async expectHomePageIsNotOpened() {
+    await expect(this.page).not.toHaveURL(HomePage.url);
+    await expect(this.userProfileBtn).not.toBeVisible();
+  };
+};
--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,40 +1,27 @@
-  
-import { test, expect} from '@playwright/test';
+import { test } from '@playwright/test';
+import LoginPage from '../pages/LoginPage';
+import HomePage from '../pages/HomePage';
 
 test.describe('User authentication', () => {
-
   test.beforeEach(async ({ page }) => {
-    await page.goto('/');
+    await page.goto(LoginPage.url)
   });
 
-  test('login with valid credentials', async ({ page }) => {
+  test('login with valid credentials', { tag: '@smoke' },  async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    const homePage = new HomePage(page);
+    await loginPage.login(process.env.EMAIL, process.env.PASSWORD);
 
-    await page.getByPlaceholder('Email Address')
-      .fill(process.env.EMAIL);
-
-    await page.getByPlaceholder('Password')
-      .fill(process.env.PASSWORD);
-
-    await page.getByRole('button', { name: 'Log In' }).click();
-
-   // Wait for navigation
-    await expect(page).toHaveURL(/#!\/home/);
-
-    // Wait for user-visible confirmation
-    await expect(page.getByText("Your Music")).toBeVisible();
+    // Validate that home page is opened
+    await homePage.expectHomePageIsOpened();
   });
 
-  test('login with invalid password', async ({ page }) => {
+  test('login with invalid password', { tag: '@regression' }, async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    const homePage = new HomePage(page);
+    await loginPage.login(process.env.EMAIL, 'WRONG_PASSWORD');
 
-    await page.locator('input[type="email"]')
-      .fill(process.env.EMAIL);
-
-    await page.locator('input[type="password"]')
-      .fill('WRONG_PASSWORD');
-
-    await page.locator('button[type="submit"]').click();
-
-    // Verify user is NOT logged in
-    await expect(page.getByText('Your Music')).not.toBeVisible();
+    // Validate that home page is not opened
+    await homePage.expectHomePageIsNotOpened();
   });
 });
